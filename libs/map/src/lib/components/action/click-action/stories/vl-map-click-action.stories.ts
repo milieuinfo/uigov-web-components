1ec5a789e743b1a9d8d8b7645f8245ed51f675fb
wimdetroyer <wimdetroyer@gmail.com>
2023-02-08 15:31:44 +0100

    feat: KBOMG-27 - vl-map-click-action component
    - introductie vl-map-click-action component

---
+++ libs/map/src/lib/components/action/click-action/stories/vl-map-click-action.stories.ts
@@ -0,0 +1,41 @@
+import { mapClickActionArgs, mapClickActionArgTypes } from './vl-map-click-action.stories-arg';
+import { html } from 'lit';
+import '../../../../vl-map';
+import '../../../baselayer/vl-map-base-layer-grb-gray';
+import '../../../layer/vector-layer/vl-map-features-layer';
+import '../vl-map-click-action';
+import { StoryFn } from '@storybook/web-components';
+import mapClickActionDoc from './vl-map-click-action.stories-doc.mdx';
+import { VlMapClickedEvent } from '../../VlMapClickedEvent';
+
+export default {
+    title: 'map/action',
+    args: mapClickActionArgs,
+    argTypes: mapClickActionArgTypes,
+    parameters: {
+        controls: { hideNoControlsWarning: true },
+        docs: { page: mapClickActionDoc },
+    },
+};
+
+const Template: StoryFn<typeof mapClickActionArgs> = ({ onClickActionStoryBook }: typeof mapClickActionArgs) => {
+    return html`
+        <vl-map>
+            <vl-map-baselayer-grb-gray></vl-map-baselayer-grb-gray>
+            <vl-map-click-action @vl-map-clicked="${storybookTest}"></vl-map-click-action>
+        </vl-map>
+    `;
+
+    function storybookTest(evt: VlMapClickedEvent) {
+        console.log(evt);
+        onClickActionStoryBook({
+            evt,
+            coordinate: evt.coordinate,
+            resolution: evt.resolution,
+            projection: evt.projection,
+        });
+    }
+};
+
+export const clickActionDefault = Template.bind({});
+clickActionDefault.storyName = 'vl-map-click-action - default';
