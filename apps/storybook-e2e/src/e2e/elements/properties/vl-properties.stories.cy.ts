50ff1548bda902f0bf71cea46ee7cf89bdf338d6
Kristof Spaas <kristof.spaas@krispsolutions.be>
2023-02-14 11:09:00 +0100

    feat: UIG-2329 - properties verbeteringen
    VlProperties Storybook en Cypress verbeterd
    elements lib module-uig.d.ts toegevoegd
    VlProgressBar kleine refactor story template

--- apps/storybook-e2e/src/e2e/elements/properties/vl-properties.stories.cy.ts
+++ apps/storybook-e2e/src/e2e/elements/properties/vl-properties.stories.cy.ts
@@ -1,18 +1,76 @@
 const propertiesUrl = 'http://localhost:8080/iframe.html?id=elements-properties--properties-default&viewMode=story';
+const propertiesColumnUrl =
+    'http://localhost:8080/iframe.html?id=elements-properties--properties-column&viewMode=story';
 
-describe('story vl-properties', () => {
-    it('should be able to see properties and their values', () => {
-        cy.visit(`${propertiesUrl}`);
-        cy.getDataCy('property-term-1').should('have.class', 'vl-properties__label').contains('Voornaam');
+const shouldSetCollapsedClass = () => {
+    cy.get('vl-properties').should('have.class', 'vl-properties--collapsed');
+};
 
-        cy.getDataCy('property-value-1').should('have.class', 'vl-properties__data').contains('Koen');
+const shouldSetFullWidthClass = () => {
+    cy.get('vl-properties').should('have.class', 'vl-properties--full-width');
+};
 
-        cy.getDataCy('property-term-2').should('have.class', 'vl-properties__label').contains('Naam');
+describe('story vl-properties default', () => {
+    it('should render properties and values', () => {
+        cy.visit(propertiesUrl);
 
-        cy.getDataCy('property-value-2').should('have.class', 'vl-properties__data').contains('Peeters');
+        cy.get('vl-properties').find('dt').should('have.class', 'vl-properties__label').contains('Voornaam');
+        cy.get('vl-properties').find('dd').should('have.class', 'vl-properties__data').contains('Koen');
+        cy.get('vl-properties').find('dt').should('have.class', 'vl-properties__label').contains('Naam');
+        cy.get('vl-properties').find('dd').should('have.class', 'vl-properties__data').contains('Peeters');
+        cy.get('vl-properties').find('dt').should('have.class', 'vl-properties__label').contains('Geslacht');
+        cy.get('vl-properties').find('dd').should('have.class', 'vl-properties__data').contains('Man');
+    });
 
-        cy.getDataCy('property-term-3').should('have.class', 'vl-properties__label').contains('Geslacht');
+    it('should set collapsed class', () => {
+        cy.visit(`${propertiesUrl}&args=collapsed:true`);
 
-        cy.getDataCy('property-value-3').should('have.class', 'vl-properties__data').contains('Man');
+        shouldSetCollapsedClass();
+    });
+
+    it('should set full-width class', () => {
+        cy.visit(`${propertiesUrl}&args=fullWidth:true`);
+
+        shouldSetFullWidthClass();
+    });
+});
+
+describe('story vl-properties column', () => {
+    it('should render properties and values', () => {
+        cy.visit(propertiesColumnUrl);
+
+        cy.get('vl-properties').find('dt').should('have.class', 'vl-properties__label').contains('Voornaam');
+        cy.get('vl-properties').find('dd').should('have.class', 'vl-properties__data').contains('Koen');
+        cy.get('vl-properties').find('dt').should('have.class', 'vl-properties__label').contains('Naam');
+        cy.get('vl-properties').find('dd').should('have.class', 'vl-properties__data').contains('Peeters');
+        cy.get('vl-properties').find('dt').should('have.class', 'vl-properties__label').contains('Geslacht');
+        cy.get('vl-properties').find('dd').should('have.class', 'vl-properties__data').contains('Man');
+        cy.get('vl-properties').find('dt').should('have.class', 'vl-properties__label').contains('Telefoon');
+        cy.get('vl-properties').find('dd').should('have.class', 'vl-properties__data').contains('000/00.00.00');
+        cy.get('vl-properties').find('dt').should('have.class', 'vl-properties__label').contains('Gsm-nummer');
+        cy.get('vl-properties').find('dd').should('have.class', 'vl-properties__data').contains('000/00.00.00');
+        cy.get('vl-properties').find('dt').should('have.class', 'vl-properties__label').contains('E-mailadres');
+        cy.get('vl-properties')
+            .find('dd')
+            .should('have.class', 'vl-properties__data')
+            .contains('koen.peeters@outlook.be');
+    });
+
+    it('should set collapsed class', () => {
+        cy.visit(`${propertiesColumnUrl}&args=collapsed:true`);
+
+        shouldSetCollapsedClass;
+    });
+
+    it('should set full class on column', () => {
+        cy.visit(`${propertiesColumnUrl}&args=full:true`);
+
+        cy.get('vl-properties').find('[is="vl-properties-column"]').should('have.class', 'vl-properties__column--full');
+    });
+
+    it('should set full-width class', () => {
+        cy.visit(`${propertiesColumnUrl}&args=fullWidth:true`);
+
+        shouldSetFullWidthClass();
     });
 });
