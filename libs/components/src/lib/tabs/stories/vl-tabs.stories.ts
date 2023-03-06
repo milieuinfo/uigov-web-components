5e88a4f182c46740f6e2304ba3bb8550aeb9ebf1
Kristof Spaas <kristof.spaas@vlaanderen.be>
2023-03-06 13:39:52 +0100

    feat: UIG-2135 - vl-tabs disable-links attribuut
    VlTabs disable tab links
    VlTabs stories en story args verbeterd

--- libs/components/src/lib/tabs/stories/vl-tabs.stories.ts
+++ libs/components/src/lib/tabs/stories/vl-tabs.stories.ts
@@ -1,29 +1,39 @@
-import { html } from 'lit-html';
+import { html, nothing } from 'lit-html';
 import '../vl-tabs.component';
+import { Meta, StoryFn } from '@storybook/web-components';
 import { tabsArgs, tabsArgTypes } from './vl-tabs.stories-arg';
 
 export default {
     title: 'Components/tabs',
     args: tabsArgs,
     argTypes: tabsArgTypes,
-    parameters: {
-        controls: { hideNoControlsWarning: true },
-    },
-};
+} as Meta<typeof tabsArgs>;
 
-export const tabsDefault = ({ alt, responsiveLabel }: typeof tabsArgs) => html`
-    <vl-tabs data-cy="tabs" ?data-vl-alt=${alt} ?data-vl-responsive-label=${responsiveLabel}>
-        <vl-tabs-pane data-vl-id="trein" data-vl-title="Trein" data-cy="tab-pane-1">
+export const tabsDefault: StoryFn<typeof tabsArgs> = ({
+    activeTab,
+    alt,
+    disableLinks,
+    responsiveLabel,
+    onChangeActiveTab,
+}) => html`
+    <vl-tabs
+        data-vl-active-tab=${activeTab || nothing}
+        ?data-vl-alt=${alt}
+        ?data-vl-responsive-label=${responsiveLabel}
+        ?data-vl-disable-links=${disableLinks}
+        @change=${(event: CustomEvent) => onChangeActiveTab(event.detail)}
+    >
+        <vl-tabs-pane data-vl-id="trein" data-vl-title="Trein">
             Nullam quis risus eget urna mollis ornare vel eu leo. Duis mollis, est non commodo luctus, nisi erat
             porttitor ligula, eget lacinia odio sem nec elit. Donec sed odio dui. Integer posuere erat a ante venenatis
             dapibus posuere velit aliquet.
         </vl-tabs-pane>
-        <vl-tabs-pane data-vl-id="metro-tram-bus" data-vl-title="Metro, tram en bus" data-cy="tab-pane-2">
+        <vl-tabs-pane data-vl-id="metro" data-vl-title="Metro, tram en bus">
             Donec sed odio dui. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Etiam porta sem
             malesuada magna mollis euismod. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Lorem ipsum
             dolor sit amet, consectetur adipiscing elit.
         </vl-tabs-pane>
-        <vl-tabs-pane data-vl-id="fiets" data-vl-title="Fiets" data-cy="tab-pane-3">
+        <vl-tabs-pane data-vl-id="fiets" data-vl-title="Fiets">
             Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Aenean eu
             leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cras justo odio, dapibus ac facilisis
             in, egestas eget quam. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
@@ -32,19 +42,32 @@ export const tabsDefault = ({ alt, responsiveLabel }: typeof tabsArgs) => html`
 `;
 tabsDefault.storyName = 'vl-tabs - default';
 
-export const tabsReactive = ({ alt, responsiveLabel }: typeof tabsArgs) =>
+export const tabsReactive: StoryFn<typeof tabsArgs> = ({
+    activeTab,
+    alt,
+    disableLinks,
+    responsiveLabel,
+    onChangeActiveTab,
+}) =>
     html`
         <button is="vl-button" onclick="addPane()">Pane toevoegen</button>
-        <vl-tabs id="tabs" ?data-vl-alt=${alt} ?data-vl-responsive-label=${responsiveLabel}>
-            <vl-tabs-pane data-vl-id="metro-tram-bus" data-vl-title="Metro, tram en bus">
+        <vl-tabs
+            id="tabs"
+            data-vl-active-tab=${activeTab || nothing}
+            ?data-vl-alt=${alt}
+            ?data-vl-responsive-label=${responsiveLabel}
+            ?data-vl-disable-links=${disableLinks}
+            @change=${(event: CustomEvent) => onChangeActiveTab(event.detail)}
+        >
+            <vl-tabs-pane data-vl-id="trein" data-vl-title="Trein">
+                Nullam quis risus eget urna mollis ornare vel eu leo. Duis mollis, est non commodo luctus, nisi erat
+                porttitor ligula, eget lacinia odio sem nec elit. Donec sed odio dui. Integer posuere erat a ante
+                venenatis dapibus posuere velit aliquet.
+            </vl-tabs-pane>
+            <vl-tabs-pane data-vl-id="metro" data-vl-title="Metro, tram en bus">
                 Donec sed odio dui. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Etiam porta sem
                 malesuada magna mollis euismod. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Lorem
                 ipsum dolor sit amet, consectetur adipiscing elit.
-            </vl-tabs-pane>
-            <vl-tabs-pane data-vl-id="fiets" data-vl-title="Fiets">
-                Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Aenean
-                eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cras justo odio, dapibus ac
-                facilisis in, egestas eget quam. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
             </vl-tabs-pane>
         </vl-tabs>
         <script>
@@ -52,14 +75,15 @@ export const tabsReactive = ({ alt, responsiveLabel }: typeof tabsArgs) =>
             const addPane = () => {
                 const div = document.createElement('div');
                 div.innerHTML =
-                    '<vl-tabs-pane data-vl-id="trein-' +
+                    '<vl-tabs-pane data-vl-id="fiets-' +
                     index +
-                    '" data-vl-title="Trein ' +
+                    '" data-vl-title="Fiets ' +
                     index +
                     '">TEST ' +
                     index +
                     '</vl-tabs-pane>';
                 document.getElementById('tabs').appendChild(div.firstElementChild);
+                index++;
             };
         </script>
     `;
