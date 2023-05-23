0a9cadba2361cde1dce11b1a7b9394d11259fda4
kspeltix <kris.speltincx@vlaanderen.be>
2023-05-23 14:43:11 +0200

    feat: UIG-2393 - vl-textarea - stylen van de inhoud in rich-text mode
    - technisch wordt er nu verwezen naar de specifiek hiervoor gepubliceerde css op de CDN
    - best practice om dezelfde versie van @domg/govflanders-style in de package.json te gebruiken
    - daarnaast een kleine opkuis in de css van playground en storybook: de vlElementsStyle uit @domg-wc/elements bevat alle styling die nodig is op root niveau

--- apps/storybook/.storybook/preview.ts
+++ apps/storybook/.storybook/preview.ts
@@ -1,15 +1,8 @@
 import { formatHTML } from '@domg-wc/common-utilities';
-import { CSSResult } from 'lit';
 import './docs-styling.css';
-import { resetStyle, baseStyle, typographyStyle } from '@domg/govflanders-style/common';
-import elementsStyle from '../../../libs/elements/src/lib/vl-elements.uig-css';
+import vlElementsStyle from '../../../libs/elements/src/lib/vl-elements.uig-css';
 
-document.adoptedStyleSheets = [
-    resetStyle.styleSheet,
-    typographyStyle.styleSheet,
-    baseStyle.styleSheet,
-    ...elementsStyle.map((style) => style.styleSheet),
-];
+document.adoptedStyleSheets = [...vlElementsStyle.map((style) => style.styleSheet)];
 
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
