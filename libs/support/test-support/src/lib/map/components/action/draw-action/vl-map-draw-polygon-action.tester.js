7bf60d1e5b078a5407334e03b17d4aa54cd2b617
Sander Kleykens <sander@kleykens.com>
2023-05-31 16:42:33 +0200

    fix: UIG-2510 - vl-map - wacht tussen move en click in vl-map-draw-line-action.tester en vl-map-draw-polygon-action.tester

--- libs/support/test-support/src/lib/map/components/action/draw-action/vl-map-draw-polygon-action.tester.js
+++ libs/support/test-support/src/lib/map/components/action/draw-action/vl-map-draw-polygon-action.tester.js
@@ -17,12 +17,14 @@ export class VlMapDrawPolygonActionTester extends VlMapDrawActionTester {
             for (let i = 0; i < coordinates.length; i++) {
                 const coordinate = coordinates[i];
                 const pixel = await map.getPixelFromCoordinate([coordinate.x, coordinate.y]);
+                await actions.move({origin: map, x: pixel.x, y: pixel.y}).pause(1000); // wacht bij een move/klik aangezien te snel klikken problematisch is
                 if (i + 1 < coordinates.length) {
-                    await actions.move({origin: map, x: pixel.x, y: pixel.y}).click();
+                    await actions.click();
                 } else {
-                    await actions.move({origin: map, x: pixel.x, y: pixel.y}).doubleClick().perform();
+                    await actions.doubleClick();
                 }
             }
+            await actions.perform();
         });
     }
 }
