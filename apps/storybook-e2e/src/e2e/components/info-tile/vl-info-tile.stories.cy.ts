21f30f06ac00969f656d69479c0f2f216a2f7fdd
Kristof Spaas <kristof.spaas@vlaanderen.be>
2023-05-31 11:23:37 +0200

    fix: UIG-2487 - vl-info-tile - positie pijltje
    fix positie van pijltje bij openen
    storybook & cypress verbeteringen

--- apps/storybook-e2e/src/e2e/components/info-tile/vl-info-tile.stories.cy.ts
+++ apps/storybook-e2e/src/e2e/components/info-tile/vl-info-tile.stories.cy.ts
@@ -1,41 +1,88 @@
 const infoTileUrl = 'http://localhost:8080/iframe.html?id=components-info-tile--info-tile-default&viewMode=story';
+const infoTileToggleableUrl =
+    'http://localhost:8080/iframe.html?id=components-info-tile--info-tile-toggleable&viewMode=story';
 
-describe('story vl-info-tile', () => {
+describe('story vl-info-tile - default', () => {
+    it('should be accessible', () => {
+        cy.visitWithA11y(infoTileUrl);
+
+        cy.checkA11y('vl-info-tile');
+    });
+
     it('should contain a title', () => {
-        cy.visit(`${infoTileUrl}`);
-        cy.getDataCy('info-tile').find('span[slot="title"]').contains('Broos Deprez');
+        cy.visit(infoTileUrl);
+
+        cy.get('vl-info-tile').shadow().find('slot[name="title"]');
+        cy.get('vl-info-tile').find('span[slot="title"]').contains('Broos Deprez');
     });
 
     it('should contain a subtitle', () => {
-        cy.visit(`${infoTileUrl}`);
-        cy.getDataCy('info-tile').find('span[slot="subtitle"]').contains('Uw zoon (19.05.2005)');
+        cy.visit(infoTileUrl);
+
+        cy.get('vl-info-tile').shadow().find('slot[name="subtitle"]');
+        cy.get('vl-info-tile').find('span[slot="subtitle"]').contains('Uw zoon (19.05.2005)');
     });
 
     it('should contain content', () => {
-        cy.visit(`${infoTileUrl}`);
-        cy.getDataCy('info-tile')
+        cy.visit(infoTileUrl);
+
+        cy.get('vl-info-tile').shadow().find('slot[name="content"]');
+        cy.get('vl-info-tile')
+            .find('div[slot="content"]')
+            .contains('De studietoelage voor Broos Deprez werd toegekend.');
+    });
+});
+
+describe('story vl-info-tile - toggleable', () => {
+    it('should be accessible', () => {
+        cy.visitWithA11y(infoTileToggleableUrl);
+
+        cy.checkA11y('vl-info-tile');
+    });
+
+    it('should contain a title', () => {
+        cy.visit(infoTileToggleableUrl);
+
+        cy.get('vl-info-tile').shadow().find('slot[name="title"]');
+        cy.get('vl-info-tile').find('span[slot="title"]').contains('Broos Deprez');
+    });
+
+    it('should contain a subtitle', () => {
+        cy.visit(infoTileToggleableUrl);
+
+        cy.get('vl-info-tile').shadow().find('slot[name="subtitle"]');
+        cy.get('vl-info-tile').find('span[slot="subtitle"]').contains('Uw zoon (19.05.2005)');
+    });
+
+    it('should contain content', () => {
+        cy.visit(infoTileToggleableUrl);
+
+        cy.get('vl-info-tile').shadow().find('slot[name="content"]');
+        cy.get('vl-info-tile')
             .find('div[slot="content"]')
             .contains('De studietoelage voor Broos Deprez werd toegekend.');
     });
 
-    it('should be able to open and close a toggeble info tile', () => {
-        cy.visit(`${infoTileUrl}&args=toggleable:true`);
-        cy.getDataCy('info-tile')
-            .shadow()
-            .find('button.vl-toggle')
-            .should('have.attr', 'aria-expanded', 'false')
-            .click()
-            .should('have.attr', 'aria-expanded', 'true')
-            .click()
-            .should('have.attr', 'aria-expanded', 'false');
+    it('should be able to open and close', () => {
+        cy.visitWithA11y(infoTileToggleableUrl);
+
+        cy.get('vl-info-tile').shadow().find('.vl-info-tile').should('not.have.class', 'js-vl-accordion--open');
+        cy.get('vl-info-tile').shadow().find('button.vl-toggle').should('have.attr', 'aria-expanded', 'false');
+        cy.checkA11y('vl-info-tile');
+        cy.get('vl-info-tile').shadow().find('button.vl-toggle').click();
+        cy.get('vl-info-tile').shadow().find('.vl-info-tile').should('have.class', 'js-vl-accordion--open');
+        cy.get('vl-info-tile').shadow().find('button.vl-toggle').should('have.attr', 'aria-expanded', 'true');
+        cy.checkA11y('vl-info-tile');
+        cy.get('vl-info-tile').shadow().find('button.vl-toggle').click();
+        cy.get('vl-info-tile').shadow().find('.vl-info-tile').should('not.have.class', 'js-vl-accordion--open');
+        cy.get('vl-info-tile').shadow().find('button.vl-toggle').should('have.attr', 'aria-expanded', 'false');
+        cy.checkA11y('vl-info-tile');
     });
 
-    it('should be able immediately see the content of an info tile after it opens automatically', () => {
-        cy.visit(`${infoTileUrl}&args=autoOpen:true;toggleable:true`);
-        cy.getDataCy('info-tile').shadow().find('.js-vl-accordion--open');
+    it('should open the info-tile automatically', () => {
+        cy.visit(`${infoTileToggleableUrl}&args=autoOpen:true`);
 
-        cy.getDataCy('info-tile')
-            .find('div[slot="content"]')
-            .contains('De studietoelage voor Broos Deprez werd toegekend.');
+        cy.get('vl-info-tile').shadow().find('.vl-info-tile').should('have.class', 'js-vl-accordion--open');
+        cy.get('vl-info-tile').shadow().find('button.vl-toggle').should('have.attr', 'aria-expanded', 'true');
     });
 });
