29c722f93e1ee9d1f44648d2071606858475d753
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2023-07-23 11:55:41 +0200

    fix: UIG-2560 - vl-map - storybook tests voor GRB lagen
    De oude URl voor grafische webdiensten van Digitaal Vlaanderen werd uitgefaseerd. Hierdoor zag je geen lagen meer in de Storybook.
    De URL werd aangepast naar de nieuwe waardoor deze weer resultaat geven.

--- apps/storybook-e2e/src/e2e/map/layer/wms-layer/tiled-wms-layer/vl-map-tiled-wms-layer.stories-dom.cy.ts
+++ apps/storybook-e2e/src/e2e/map/layer/wms-layer/tiled-wms-layer/vl-map-tiled-wms-layer.stories-dom.cy.ts
@@ -2,7 +2,7 @@ const mapTiledWmsLayerUrl =
     'http://localhost:8080/iframe.html?id=map-layer-wms-layer-tiled-wms-layer--map-tiled-wms-layer-default&viewMode=story';
 
 describe('story vl-map-tiled-wms-layer default', () => {
-    const wmsUrl = 'https://geoservices.informatievlaanderen.be/raadpleegdiensten/GRB/wms';
+    const wmsUrl = 'https://geo.api.vlaanderen.be/GRB/wms';
 
     it('should fetch WMS layer', () => {
         cy.visit(mapTiledWmsLayerUrl);
