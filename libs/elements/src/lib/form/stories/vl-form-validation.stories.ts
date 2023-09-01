0cfd878029f59dc6d1c650e10736ed4cbd2242e0
Karim Dehbi <karim.dehbi@gmail.com>
2023-09-01 13:14:35 +0200

    fix: UIG-2649 - vl-form - requestValidation - config wordt nu correct doorgegeven
    Storybook verbeterd & cypress test toegevoegd die nieuwe functionaliteit aftest.

--- libs/elements/src/lib/form/stories/vl-form-validation.stories.ts
+++ libs/elements/src/lib/form/stories/vl-form-validation.stories.ts
@@ -8,6 +8,7 @@ import { formArgs, formArgTypes } from './vl-form.stories-arg';
 import formValidationDoc from './vl-form-validation.stories-doc.mdx';
 import { Meta } from '@storybook/web-components';
 import { story } from '@domg-wc/common-storybook';
+import { VlFormElement } from '@domg-wc/elements';
 
 export default {
     title: 'Elements/form',
@@ -223,4 +224,86 @@ formValidationEscapeFieldNames.storyName = 'vl-form - validation escape field na
 formValidationEscapeFieldNames.args = {
     validate: true,
     escapeFieldNames: true,
+};
+
+export const formValidationWithoutSubmit = story(formArgs, ({ validate, nativeValidation, escapeFieldNames }) => {
+    let vlForm: HTMLFormElement & VlFormElement;
+    customElements.whenDefined('vl-form').then(() => {
+        vlForm = <HTMLFormElement & VlFormElement>document.querySelector('form[is="vl-form"]');
+    });
+
+    return html`
+        <div style="max-width: 800px">
+            <form
+                is="vl-form"
+                ?data-vl-validate=${validate}
+                ?data-vl-native-validation=${nativeValidation}
+                ?data-vl-escape-field-names=${escapeFieldNames}
+            >
+                <div is="vl-form-grid" data-vl-is-stacked>
+                    <div is="vl-form-column" data-vl-size="3">
+                        <label is="vl-form-label" for="name" data-vl-block>
+                            Naam
+                            <span is="vl-form-annotation-span">(verplicht)</span>
+                        </label>
+                    </div>
+                    <div is="vl-form-column" data-vl-size="9">
+                        <input
+                            id="name"
+                            name="name"
+                            autocomplete="name"
+                            is="vl-input-field"
+                            data-vl-block
+                            data-vl-required
+                            data-vl-error-message="Geef een naam in."
+                            data-vl-error-placeholder="name-error"
+                        />
+                        <p is="vl-form-validation-message" data-vl-error data-vl-error-id="name-error"></p>
+                    </div>
+
+                    <div is="vl-form-column" data-vl-size="3">
+                        <label is="vl-form-label" for="firstname" data-vl-block> Voornaam </label>
+                    </div>
+                    <div is="vl-form-column" data-vl-size="9">
+                        <input
+                            id="firstname"
+                            name="firstname"
+                            autocomplete="given-name"
+                            is="vl-input-field"
+                            data-vl-block
+                            data-vl-required
+                            data-vl-error-message="Geef een voornaam in."
+                            data-vl-error-placeholder="firstname-error"
+                        />
+                        <p is="vl-form-validation-message" data-vl-error data-vl-error-id="firstname-error"></p>
+                    </div>
+
+                    <div is="vl-form-column" data-vl-size="9" data-vl-push="3">
+                        <div is="vl-action-group">
+                            <button
+                                id="validate-without-submit"
+                                is="vl-button"
+                                type="button"
+                                data-vl-tertiary
+                                @click=${() => {
+                                    vlForm?.requestValidation();
+                                    console.log('vlForm.checkValidity', vlForm?.checkValidity());
+                                }}
+                            >
+                                Valideer zonder submit
+                            </button>
+                            <a is="vl-link" href="#">
+                                <span is="vl-icon" data-vl-icon="cross" data-vl-before></span>
+                                Annuleren
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    `;
+});
+formValidationWithoutSubmit.storyName = 'vl-form - validation optional';
+formValidationWithoutSubmit.args = {
+    validate: true,
 };
