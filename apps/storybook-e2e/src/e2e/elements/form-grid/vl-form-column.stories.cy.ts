86ab14aae98b35092a47c313101beb0459065607
Kristof Spaas <kristof.spaas@vlaanderen.be>
2023-09-07 14:01:55 +0200

    feat: UIG-2603 - vl-column - push attributen per size
    Push attributen per size toegevoegd
    Storybook verbeterd

--- apps/storybook-e2e/src/e2e/elements/form-grid/vl-form-column.stories.cy.ts
+++ apps/storybook-e2e/src/e2e/elements/form-grid/vl-form-column.stories.cy.ts
@@ -2,14 +2,14 @@ const formGridDefaultUrl = 'http://localhost:8080/iframe.html?id=elements-form-g
 
 describe('story vl-form-column', () => {
     it('should contain form with a 8/12 form column in it', () => {
-        cy.visit(`${formGridDefaultUrl}`);
-        cy.getDataCy('form').getDataCy('form-column').should('have.class', 'vl-form-col--8-12');
+        cy.visit(formGridDefaultUrl);
+        cy.get('form[is="vl-form"]').find('div[is="vl-form-column"]').should('have.class', 'vl-form-col--8-12');
     });
 
     it("should contain form with a 8/12 form column in it that's pushed 2/12 columns", () => {
         cy.visit(`${formGridDefaultUrl}&args=push:2`);
-        cy.getDataCy('form')
-            .getDataCy('form-column')
+        cy.get('div[is="vl-form-grid"]')
+            .find('div[is="vl-form-column"]')
             .should('have.class', 'vl-form-col--8-12')
             .should('have.class', 'vl-form-push--2-12');
     });
--- libs/elements/src/lib/grid/stories/vl-grid-column.stories-arg.ts
+++ libs/elements/src/lib/grid/stories/vl-grid-column.stories-arg.ts
@@ -1,110 +1,138 @@
-export const gridColumnBaseArgs = {
+import { CATEGORIES, TYPES } from '@domg-wc/common-storybook';
+import { ArgTypes } from '@storybook/web-components';
+
+export const gridColumnArgs = {
     size: 8,
     maxSize: 12,
+    push: null,
     mediumSize: 10,
     mediumMaxSize: 12,
+    mediumPush: null,
     smallSize: 12,
     smallMaxSize: 12,
+    smallPush: null,
     extraSmallSize: 12,
     extraSmallMaxSize: 12,
-    push: 0,
+    extraSmallPush: null,
 };
 
-export const gridColumnDefaultArgs = {
-    ...gridColumnBaseArgs,
-    content:
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores assumenda dignissimos doloremque eos est eveniet fugiat illo illum impedit, libero nam, omnis optio praesentium qui quod ratione vel voluptas voluptatibus?',
-};
-
-export const gridColumnBaseArgTypes = {
+export const gridColumnArgTypes: ArgTypes<typeof gridColumnArgs> = {
     size: {
         name: 'data-vl-size',
         description:
-            'The number (numerator) of the maximum (denominator) that will be taken on large screens, typically desktop.',
+            'Het aantal (teller) van het maximum (noemer) dat genomen zal worden bij grote schermen, typisch desktop.',
         table: {
-            type: { summary: 'number' },
-            category: 'Attributes',
-            defaultValue: { summary: 8 },
+            type: { summary: TYPES.NUMBER },
+            category: CATEGORIES.ATTRIBUTES,
+            defaultValue: { summary: gridColumnArgs.size },
         },
     },
     maxSize: {
         name: 'data-vl-max-size',
-        description: 'The maximum (denominator) that will be evaluated against on large screens, typically desktop.',
+        description: 'Het maximum (noemer) waartegen zal geevalueerd worden bij grote schermen, typisch desktop.',
         table: {
-            type: { summary: 'number' },
-            category: 'Attributes',
-            defaultValue: { summary: 12 },
+            type: { summary: TYPES.NUMBER },
+            category: CATEGORIES.ATTRIBUTES,
+            defaultValue: { summary: gridColumnArgs.maxSize },
+        },
+    },
+    push: {
+        name: 'data-vl-push',
+        description:
+            'Het aantal (teller) van het maximum (noemer) partities te verschuiven bij grote schermen, typisch desktop.',
+        control: 'number',
+        table: {
+            type: { summary: TYPES.NUMBER },
+            category: CATEGORIES.ATTRIBUTES,
+            defaultValue: { summary: gridColumnArgs.push },
         },
     },
     mediumSize: {
         name: 'data-vl-medium-size',
         description:
-            'The number (numerator) of the maximum (denominator) that will be taken on medium screens, typically tablet.',
+            'Het aantal (teller) van het maximum (noemer) dat genomen zal worden bij medium schermen, typisch tablet.',
         table: {
-            type: { summary: 'number' },
-            category: 'Attributes',
-            defaultValue: { summary: 10 },
+            type: { summary: TYPES.NUMBER },
+            category: CATEGORIES.ATTRIBUTES,
+            defaultValue: { summary: gridColumnArgs.mediumSize },
         },
     },
     mediumMaxSize: {
         name: 'data-vl-medium-max-size',
-        description: 'The maximum (denominator) that will be evaluated against on medium screens, typically tablet.',
+        description: 'Het maximum (noemer) waartegen zal geevalueerd worden bij medium schermen, typisch tablet.',
         table: {
-            type: { summary: 'number' },
-            category: 'Attributes',
-            defaultValue: { summary: 12 },
+            type: { summary: TYPES.NUMBER },
+            category: CATEGORIES.ATTRIBUTES,
+            defaultValue: { summary: gridColumnArgs.mediumMaxSize },
+        },
+    },
+    mediumPush: {
+        name: 'data-vl-medium-push',
+        description:
+            'Het aantal (teller) van het maximum (noemer) partities te verschuiven bij medium schermen, typisch tablet.',
+        control: 'number',
+        table: {
+            type: { summary: TYPES.NUMBER },
+            category: CATEGORIES.ATTRIBUTES,
+            defaultValue: { summary: gridColumnArgs.mediumPush },
         },
     },
     smallSize: {
         name: 'data-vl-small-size',
         description:
-            'The number (numerator) of the maximum (denominator) that will be taken on small screens, typically mobile.',
+            'Het aantal (teller) van het maximum (noemer) dat genomen zal worden bij kleine schermen, typisch mobiel.',
         table: {
-            type: { summary: 'number' },
-            category: 'Attributes',
-            defaultValue: { summary: 12 },
+            type: { summary: TYPES.NUMBER },
+            category: CATEGORIES.ATTRIBUTES,
+            defaultValue: { summary: gridColumnArgs.smallSize },
         },
     },
     smallMaxSize: {
         name: 'data-vl-small-max-size',
-        description: 'The maximum (denominator) that will be evaluated against on small screens, typically mobile. ',
+        description: 'Het maximum (noemer) waartegen zal geevalueerd worden bij kleine schermen, typisch mobiel.',
         table: {
-            type: { summary: 'number' },
-            category: 'Attributes',
-            defaultValue: { summary: 12 },
+            type: { summary: TYPES.NUMBER },
+            category: CATEGORIES.ATTRIBUTES,
+            defaultValue: { summary: gridColumnArgs.smallMaxSize },
+        },
+    },
+    smallPush: {
+        name: 'data-vl-small-push',
+        description:
+            'Het aantal (teller) van het maximum (noemer) partities te verschuiven bij kleine schermen, typisch mobiel.',
+        control: 'number',
+        table: {
+            type: { summary: TYPES.NUMBER },
+            category: CATEGORIES.ATTRIBUTES,
+            defaultValue: { summary: gridColumnArgs.smallPush },
         },
     },
     extraSmallSize: {
         name: 'data-vl-extra-small-size',
-        description: 'The number (numerator) of the maximum (denominator) that will be taken on very small screens.',
+        description: 'Het aantal (teller) van het maximum (noemer) dat genomen zal worden bij zeer kleine schermen.',
         table: {
-            type: { summary: 'number' },
-            category: 'Attributes',
-            defaultValue: { summary: 12 },
+            type: { summary: TYPES.NUMBER },
+            category: CATEGORIES.ATTRIBUTES,
+            defaultValue: { summary: gridColumnArgs.extraSmallSize },
         },
     },
     extraSmallMaxSize: {
         name: 'data-vl-extra-small-max-size',
-        description: 'The maximum (denominator) against which to evaluate for very small screens.',
+        description: 'Het maximum (noemer) waartegen zal geevalueerd worden bij zeer kleine schermen.',
         table: {
-            type: { summary: 'number' },
-            category: 'Attributes',
-            defaultValue: { summary: 12 },
+            type: { summary: TYPES.NUMBER },
+            category: CATEGORIES.ATTRIBUTES,
+            defaultValue: { summary: gridColumnArgs.extraSmallMaxSize },
         },
     },
-    push: {
-        name: 'data-vl-push',
-        description: 'Number of partitions to move.',
+    extraSmallPush: {
+        name: 'data-vl-extra-small-push',
+        description: 'Het aantal (teller) van het maximum (noemer) partities te verschuiven bij zeer kleine schermen.',
+        control: 'number',
         table: {
-            type: { summary: 'number' },
-            category: 'Attributes',
+            type: { summary: TYPES.NUMBER },
+            category: CATEGORIES.ATTRIBUTES,
+            defaultValue: { summary: gridColumnArgs.extraSmallPush },
         },
-    },
-};
-
-export const gridColumnDefaultArgTypes = {
-    ...gridColumnBaseArgTypes,
-    content: {
-        name: 'content (for demo purposes)',
     },
 };
