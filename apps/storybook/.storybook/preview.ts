b4e60815f28732c4d6782d3dbdb7b50e564c8797
kspeltix <kris.speltincx@vlaanderen.be>
2023-09-08 12:20:10 +0200

    feat: UIG-2655 - storybook documentatie mbt imports en tree-shaking

--- apps/storybook/.storybook/preview.ts
+++ apps/storybook/.storybook/preview.ts
@@ -18,3 +18,24 @@ export const parameters = {
         transformSource: (input: string) => formatHTML(filterOutClasses(input)),
     },
 };
+
+// Hacky way of clicking on Docs button on first load of page.
+// https://github.com/storybookjs/storybook/issues/13128
+function clickDocsButtonOnFirstLoad() {
+    window.removeEventListener('load', clickDocsButtonOnFirstLoad);
+    try {
+        const docsButtonSelector = window.parent.document.evaluate(
+            "//button[contains(., 'Docs')]",
+            window.parent.document,
+            null,
+            XPathResult.ANY_TYPE,
+            null
+        );
+        const button = docsButtonSelector.iterateNext();
+        button.click();
+    } catch (error) {
+        // Do nothing if it wasn't able to click on Docs button.
+    }
+}
+
+window.addEventListener('load', clickDocsButtonOnFirstLoad);
