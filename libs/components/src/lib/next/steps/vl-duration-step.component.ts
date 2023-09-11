73ac7679a44317e43e9496c74b171f8beaf6ac95
Kristof Spaas <kristof.spaas@vlaanderen.be>
2023-09-11 14:44:50 +0200

    feat: UIG-2590 - vl-steps-next - introductie component
    vl-steps-next component introductie
    cypress en storybook toegevoegd

---
+++ libs/components/src/lib/next/steps/vl-duration-step.component.ts
@@ -0,0 +1,27 @@
+import { BaseLitElement } from '@domg-wc/common-utilities';
+import { resetStyle } from '@domg/govflanders-style/common';
+import { vlElementsStyle } from '@domg-wc/elements';
+import { CSSResult, TemplateResult, html } from 'lit';
+import { customElement } from 'lit/decorators.js';
+import { stepsStyle } from '@domg/govflanders-style/component';
+
+@customElement('vl-duration-step-next')
+export class VlDurationStepComponent extends BaseLitElement {
+    static get styles(): (CSSResult | CSSResult[])[] {
+        return [resetStyle, vlElementsStyle, stepsStyle];
+    }
+
+    render(): TemplateResult {
+        return html`
+            <li role="listitem" class="vl-duration-step">
+                <slot></slot>
+            </li>
+        `;
+    }
+}
+
+declare global {
+    interface HTMLElementTagNameMap {
+        'vl-duration-step-next': VlDurationStepComponent;
+    }
+}
