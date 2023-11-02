98e89b71536aba013f92bad5190e72b988a59429
Karim Dehbi <karim.dehbi@gmail.com>
2023-11-02 17:13:07 +0100

    feat: UIG-2731 - vl-info-tile - center attribuut toegevoegd
    Cypress testen uitgebreid & storybook testen gemigreerd.

--- libs/components/src/info-tile/vl-info-tile.component.ts
+++ libs/components/src/info-tile/vl-info-tile.component.ts
@@ -1,20 +1,26 @@
-import { BaseElementOfType, webComponent } from '@domg-wc/common-utilities';
+import { BaseHTMLElement, VL, webComponent } from '@domg-wc/common-utilities';
 import { accordionStyle, iconStyle, infoTileStyle, linkStyle, toggleStyle } from '@domg/govflanders-style/component';
-import { resetStyle } from '@domg/govflanders-style/common';
+import { baseStyle, resetStyle } from '@domg/govflanders-style/common';
 import infoTileUigStyle from './vl-info-tile.uig-css';
+import '@govflanders/vl-ui-util/dist/js/util.js';
+import '@govflanders/vl-ui-accordion/dist/js/accordion.js';
+import 'reflect-metadata';
+import { vlElementsStyle } from '@domg-wc/elements';
 
-declare const vl: any;
+declare const vl: VL;
 
 @webComponent('vl-info-tile')
-export class VlInfoTile extends BaseElementOfType(HTMLElement) {
+export class VlInfoTile extends BaseHTMLElement {
     static get _observedAttributes() {
-        return ['auto-open', 'toggleable'];
+        return ['auto-open', 'toggleable', 'center'];
     }
 
     constructor() {
         super(`
           <style>
             ${resetStyle}
+            ${baseStyle}
+            ${vlElementsStyle}
             ${infoTileStyle}
             ${infoTileUigStyle}
             ${linkStyle}
@@ -72,6 +78,10 @@ export class VlInfoTile extends BaseElementOfType(HTMLElement) {
         return this._headerWrapperElement.querySelector('#title');
     }
 
+    get _titleSlot(): HTMLSlotElement | undefined | null {
+        return this.querySelector<HTMLSlotElement>(":scope > slot[name='title']");
+    }
+
     get _titleLabelSlot() {
         return this.querySelector("[slot='title-label']");
     }
@@ -85,7 +95,7 @@ export class VlInfoTile extends BaseElementOfType(HTMLElement) {
     }
 
     toggle() {
-        this._toggleElement.click();
+        this._toggleElement?.click();
     }
 
     open() {
@@ -101,15 +111,23 @@ export class VlInfoTile extends BaseElementOfType(HTMLElement) {
     }
 
     get _toggleElement() {
-        return this._shadow.querySelector('.js-vl-accordion__toggle');
+        return this._shadow?.querySelector<HTMLElement>('.js-vl-accordion__toggle');
     }
 
     get _subtitleElement() {
-        return this._shadow.querySelector('slot[name="subtitle"]');
+        return this._shadow?.querySelector('slot[name="subtitle"]');
     }
 
     get _contentElement() {
-        return this._shadow.querySelector('slot[name="content"]');
+        return this._shadow?.querySelector('slot[name="content"]');
+    }
+
+    _centerChangedCallback(oldValue: string, newValue: string) {
+        if (newValue === null) {
+            this._element.classList.remove('vl-info-tile--center');
+        } else {
+            this._element.classList.add('vl-info-tile--center');
+        }
     }
 
     _toggleableChangedCallback(oldValue: string, newValue: string) {
@@ -141,13 +159,17 @@ export class VlInfoTile extends BaseElementOfType(HTMLElement) {
     }
 
     __preventContentClickPropagation() {
-        this._subtitleElement.addEventListener('click', (e: Event) => e.stopPropagation());
-        this._contentElement.addEventListener('click', (e: Event) => e.stopPropagation());
+        this._subtitleElement?.addEventListener('click', (e: Event) => e.stopPropagation());
+        this._contentElement?.addEventListener('click', (e: Event) => e.stopPropagation());
     }
 
     __removePreventContentClickPropagation() {
-        this._subtitleElement.removeEventListener('click', (e: Event) => e.stopPropagation());
-        this._contentElement.removeEventListener('click', (e: Event) => e.stopPropagation());
+        this._subtitleElement?.removeEventListener('click', (e: Event) => e.stopPropagation());
+        this._contentElement?.removeEventListener('click', (e: Event) => e.stopPropagation());
+    }
+
+    _hasTitleSlot() {
+        return this._titleSlot && this._titleSlot.assignedElements()?.length > 0;
     }
 
     __processAutoOpen() {
