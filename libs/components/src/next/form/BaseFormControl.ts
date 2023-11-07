57fa2d128cd4955fce0e6dc1e9f741aceef2d963
Kristof Spaas <kristof.spaas@vlaanderen.be>
2023-11-07 11:09:38 +0100

    feat: UIG-2734 - form validation - BaseFormControl
    BaseFormControl class toegevoegd met algemene logica en validatie

---
+++ libs/components/src/next/form/BaseFormControl.ts
@@ -0,0 +1,137 @@
+import { PropertyDeclarations } from 'lit';
+import {
+    FormControlMixin,
+    maxLengthValidator,
+    minLengthValidator,
+    patternValidator,
+    requiredValidator,
+} from '@open-wc/form-control';
+import { LitElement } from 'lit';
+import { submit } from '@open-wc/form-helpers';
+import { maxValueValidator, minValueValidator } from './validators';
+import { ERROR_MESSAGE_CUSTOM_TAG } from './error-message/vl-error-message.component';
+import { BaseLitElement } from '@domg-wc/common-utilities';
+
+export abstract class BaseFormControl extends FormControlMixin(BaseLitElement) {
+    id = '';
+    protected name = '';
+    protected label = '';
+    protected block = false;
+    protected required = false;
+    protected disabled = false;
+    protected isInvalid = false;
+
+    static formControlValidators = [
+        requiredValidator,
+        minLengthValidator,
+        maxLengthValidator,
+        minValueValidator,
+        maxValueValidator,
+        patternValidator,
+    ];
+
+    static shadowRootOptions = { ...LitElement.shadowRootOptions, delegatesFocus: true };
+
+    static get properties(): PropertyDeclarations {
+        return {
+            id: { type: String, reflect: true },
+            name: { type: String, reflect: true },
+            label: { type: String, reflect: true },
+            block: { type: Boolean, reflect: true },
+            required: { type: Boolean, reflect: true },
+            disabled: { type: Boolean, reflect: true },
+            isInvalid: { type: Boolean, state: true },
+        };
+    }
+
+    connectedCallback(): void {
+        super.connectedCallback();
+
+        this.addEventListener('keydown', this.onKeydown);
+        this.addEventListener('invalid', this.onInvalid);
+
+        if (!this.label) {
+            this.label = this.internals.labels[0]?.innerText || '';
+        }
+    }
+
+    disconnectedCallback(): void {
+        super.disconnectedCallback();
+
+        this.removeEventListener('keydown', this.onKeydown);
+        this.removeEventListener('invalid', this.onInvalid);
+    }
+
+    updated(changedProperties: Map<string, unknown>): void {
+        super.updated(changedProperties);
+
+        if (!changedProperties.has('isInvalid')) {
+            this.isInvalid = false;
+            this.hideErrorMessages();
+        }
+    }
+
+    abstract get validationTarget(): HTMLElement | undefined | null;
+
+    resetFormControl(): void {
+        this.isInvalid = false;
+        this.hideErrorMessages();
+        this.dispatchEvent(new Event('reset'));
+    }
+
+    private onKeydown = (event: KeyboardEvent): void => {
+        if (event.code === 'Enter') {
+            if (this.form) {
+                submit(this.form);
+            }
+        }
+    };
+
+    private onInvalid = (event: Event): void => {
+        event.preventDefault();
+
+        this.isInvalid = true;
+        this.focusFirstInvalidInput();
+        this.showErrorMessage();
+    };
+
+    private focusFirstInvalidInput(): void {
+        const firstInvalidInput = this.form.querySelector(':invalid');
+
+        if (this === firstInvalidInput) {
+            (firstInvalidInput as HTMLElement)?.focus();
+            (firstInvalidInput as HTMLElement)?.scrollIntoView();
+        }
+    }
+
+    private showErrorMessage(): void {
+        let errorState = '';
+
+        for (const key in this.validity) {
+            if (this.validity[key as keyof ValidityState]) {
+                errorState = key;
+                break;
+            }
+        }
+
+        // Zoek de error message die bij de huidige error state hoort
+        let errorMessage = this.form.querySelector(
+            `${ERROR_MESSAGE_CUSTOM_TAG}[input-id="${this.id}"][state="${errorState}"]`
+        );
+
+        // Als er geen error message is voor de huidige error state, zoek dan de algemene error message
+        if (!errorMessage) {
+            errorMessage = this.form.querySelector(`${ERROR_MESSAGE_CUSTOM_TAG}[input-id="${this.id}"]`);
+        }
+
+        errorMessage?.setAttribute('show', 'true');
+    }
+
+    private hideErrorMessages(): void {
+        const errorMessages = this.form?.querySelectorAll(`${ERROR_MESSAGE_CUSTOM_TAG}[input-id="${this.id}"]`);
+
+        errorMessages?.forEach((errorMessage) => {
+            errorMessage.removeAttribute('show');
+        });
+    }
+}
---
+++ libs/components/src/next/form/error-message/vl-error-message.component.ts
@@ -0,0 +1,36 @@
+import { customElement } from 'lit/decorators.js';
+import { BaseLitElement } from '@domg-wc/common-utilities';
+import { CSSResult, PropertyDeclarations, TemplateResult, html } from 'lit';
+import { formMessageStyle } from '@domg/govflanders-style/component';
+import { resetStyle } from '@domg/govflanders-style/common';
+
+export const ERROR_MESSAGE_CUSTOM_TAG = 'vl-error-message-next';
+
+@customElement(ERROR_MESSAGE_CUSTOM_TAG)
+export class VlErrorMessageComponent extends BaseLitElement {
+    input = '';
+    state: keyof ValidityState | null = null;
+    show = false;
+
+    static get styles(): CSSResult[] {
+        return [resetStyle, formMessageStyle];
+    }
+
+    static get properties(): PropertyDeclarations {
+        return {
+            input: { type: String, reflect: true },
+            state: { type: String, reflect: true },
+            show: { type: Boolean, reflect: true },
+        };
+    }
+
+    render(): TemplateResult {
+        return html` <p class="vl-form__error" ?hidden=${!this.show}><slot></slot></p> `;
+    }
+}
+
+declare global {
+    interface HTMLElementTagNameMap {
+        ERROR_MESSAGE_CUSTOM_TAG: VlErrorMessageComponent;
+    }
+}
