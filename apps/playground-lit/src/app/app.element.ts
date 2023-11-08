f4976f09b8a1063580369481e61698f053f61372
Karim Dehbi <karim.dehbi@gmail.com>
2023-11-08 15:59:32 +0100

    fix: UIG-2732 - vl-cascader - native & react specifieke verbeteringen toegevoegd
    Playground voorbeelden verbeterd en rechtgetrokken naar storybook voorbeelden.
    Typing verbeterd.

--- apps/playground-lit/src/app/app.element.ts
+++ apps/playground-lit/src/app/app.element.ts
@@ -1,9 +1,12 @@
 import { CSSResult, html, LitElement, TemplateResult } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { vlElementsStyle } from '@domg-wc/elements';
-import { CascaderItem, TemplateFn, VlInfoTile, VlCascaderComponent, VlAccordionComponent } from '@domg-wc/components';
+import { VlAccordionComponent, VlCascaderComponent, VlInfoTile } from '@domg-wc/components';
 import { registerWebComponents } from '@domg-wc/common-utilities';
 import appElementStyle from './app.element.css';
+import { cascaderItemTemplates } from './vl-cascader.templates';
+import { getItemList } from './vl-cascader.utils';
+import { nodeData } from './vl-cascader.data';
 
 @customElement('app-element')
 export class AppElement extends LitElement {
@@ -16,127 +19,17 @@ export class AppElement extends LitElement {
     }
 
     render(): TemplateResult {
-        const nodeData: CascaderItem[] = [
-            {
-                label: 'Level 1 - A',
-                children: [
-                    {
-                        label: 'Level 2 - A',
-                        templateType: 'stad',
-                        children: [
-                            {
-                                label: 'Level 3 - A',
-                                children: [
-                                    {
-                                        label: 'Level 4 - A',
-                                        templateType: 'provincie',
-                                    },
-                                    {
-                                        label: 'Level 4 - B',
-                                        templateType: 'provincie',
-                                    },
-                                ],
-                            },
-                            {
-                                label: 'Level 3 - B',
-                                templateType: 'provincie',
-                            },
-                        ],
-                    },
-                    {
-                        label: 'Level 2 - B',
-                        templateType: 'provincie',
-                    },
-                    {
-                        label: 'Level 2 - C',
-                        templateType: 'provincie',
-                    },
-                ],
-            },
-            {
-                label: 'Level 1 - B',
-            },
-            {
-                label: 'Level 1 - C',
-                data: {
-                    requestParams: 'level1C-id',
-                },
-                templateType: 'stad',
-            },
-        ];
-
-        const fetchNodes = async (item: CascaderItem): Promise<CascaderItem[]> => {
-            await new Promise((res) => setTimeout(res, 3000));
-            const id = item.label;
-            return [
-                {
-                    label: id + ' ' + new Date().getHours(),
-                    children: [
-                        {
-                            label: id + ' ' + new Date().getMinutes(),
-                            children: [
-                                {
-                                    label: id + ' ' + new Date().getTime(),
-                                    narrowDown: true,
-                                    data: {
-                                        requestParams: 'level1C-id',
-                                    },
-                                },
-                                {
-                                    label: '[- ' + id + ' -]',
-                                    templateType: 'provincie',
-                                },
-                            ],
-                        },
-                        {
-                            label: id + ' ' + new Date().getMinutes(),
-                        },
-                    ],
-                },
-                {
-                    label: id + ' ' + new Date().getMinutes(),
-                    children: [
-                        {
-                            label: id + ' ' + new Date().getTime(),
-                            narrowDown: true,
-                            data: {
-                                requestParams: 'level - deeper',
-                            },
-                        },
-                        {
-                            label: '[- ' + id + ' -]',
-                            templateType: 'provincie',
-                        },
-                    ],
-                },
-                {
-                    label: id + ' ' + new Date().getMinutes(),
-                },
-            ];
-        };
-
-        // kijken eventueel enkel literal meegeven
-        const nodeTemplates = new Map<string, TemplateFn>([
-            [
-                'stad',
-                (item, navigate) => html`
-                    <div>inhoud</div>
-                    <button
-                        is="vl-button"
-                        @click=${() => {
-                            navigate(item);
-                        }}
-                    >
-                        ${item.label + (item.children || item.narrowDown ? ' > there is more ' : '')}
-                    </button>
-                `,
-            ],
-        ]);
-
         return html`
             <main>
-                <vl-side-sheet data-vl-left data-vl-custom-css="" data-vl-open>
-                    <vl-cascader .items=${nodeData} .itemListFn=${fetchNodes} .templates=${nodeTemplates}>
+                <vl-side-sheet
+                    data-vl-left
+                    data-vl-custom-css=""
+                    data-vl-open
+                    data-vl-open
+                    data-vl-custom-css=${'.vl-layout {padding:0px} .vl-region{padding:10px} .vl-region:first-child{padding:0} :host #vl-side-sheet {padding:0} :host {--vl-side-sheet-width: 600px;}'}
+                >
+                    <h4 is="vl-h4" class="vl-title--has-border">Kies uit kantoren</h4>
+                    <vl-cascader .items=${nodeData} .itemListFn=${getItemList} .templates=${cascaderItemTemplates}>
                     </vl-cascader>
                 </vl-side-sheet>
             </main>
--- apps/playground-react/src/app/app.tsx
+++ apps/playground-react/src/app/app.tsx
@@ -1,9 +1,11 @@
-import { VlCascaderComponent, CascaderItem, TemplateFn, VlInfoTile, VlAccordionComponent } from '@domg-wc/components';
+import { VlAccordionComponent, VlCascaderComponent, VlInfoTile } from '@domg-wc/components';
 import { registerWebComponents } from '@domg-wc/common-utilities';
 import './app.module.css';
 import React, { DOMAttributes } from 'react';
-import { html } from 'lit-html';
 import { createComponent } from '@lit-labs/react';
+import { cascaderItemTemplates } from './vl-cascader.templates';
+import { getItemList } from './vl-cascader.utils';
+import { nodeData } from './vl-cascader.data';
 
 registerWebComponents([VlCascaderComponent, VlInfoTile, VlAccordionComponent]);
 
@@ -14,128 +16,17 @@ export const VlCascader = createComponent({
 });
 
 export function App() {
-    const nodeData: CascaderItem[] = [
-        {
-            label: 'Level 1 - A',
-            children: [
-                {
-                    label: 'Level 2 - A',
-                    templateType: 'stad',
-                    children: [
-                        {
-                            label: 'Level 3 - A',
-                            children: [
-                                {
-                                    label: 'Level 4 - A',
-                                    templateType: 'provincie',
-                                },
-                                {
-                                    label: 'Level 4 - B',
-                                    templateType: 'provincie',
-                                },
-                            ],
-                        },
-                        {
-                            label: 'Level 3 - B',
-                            templateType: 'provincie',
-                        },
-                    ],
-                },
-                {
-                    label: 'Level 2 - B',
-                    templateType: 'provincie',
-                },
-                {
-                    label: 'Level 2 - C',
-                    templateType: 'provincie',
-                },
-            ],
-        },
-        {
-            label: 'Level 1 - B',
-        },
-        {
-            label: 'Level 1 - C',
-            narrowDown: true,
-            data: {
-                requestParams: 'level1C-id',
-            },
-            templateType: 'stad',
-        },
-    ];
-
-    const fetchNodes = async (item: CascaderItem): Promise<CascaderItem[]> => {
-        await new Promise((res) => setTimeout(res, 3000));
-        const id = item.label;
-        return [
-            {
-                label: id + ' ' + new Date().getHours(),
-                children: [
-                    {
-                        label: id + ' ' + new Date().getMinutes(),
-                        children: [
-                            {
-                                label: id + ' ' + new Date().getTime(),
-                                narrowDown: true,
-                                data: {
-                                    requestParams: 'level1C-id',
-                                },
-                            },
-                            {
-                                label: '[- ' + id + ' -]',
-                                templateType: 'provincie',
-                            },
-                        ],
-                    },
-                    {
-                        label: id + ' ' + new Date().getMinutes(),
-                    },
-                ],
-            },
-            {
-                label: id + ' ' + new Date().getMinutes(),
-                children: [
-                    {
-                        label: id + ' ' + new Date().getTime(),
-                        narrowDown: true,
-                        data: {
-                            requestParams: 'level - deeper',
-                        },
-                    },
-                    {
-                        label: '[- ' + id + ' -]',
-                        templateType: 'provincie',
-                    },
-                ],
-            },
-            {
-                label: id + ' ' + new Date().getMinutes(),
-            },
-        ];
-    };
-
-    const nodeTemplates = new Map<string, TemplateFn>([
-        [
-            'stad',
-            (item, navigate) => html`
-                <div>inhoud</div>
-                <button
-                    is="vl-button"
-                    @click=${() => {
-                        navigate(item);
-                    }}
-                >
-                    ${item.label + (item.children || item.narrowDown ? ' > there is more ' : '')}
-                </button>
-            `,
-        ],
-    ]);
-
     return (
         <main>
-            <vl-side-sheet data-vl-left data-vl-custom-css="" data-vl-open>
-                <h4 is="vl-h4">Kies uit kantoren</h4>
-                <VlCascader items={nodeData} itemListFn={fetchNodes} templates={nodeTemplates}></VlCascader>
+            <vl-side-sheet
+                data-vl-left
+                data-vl-open
+                data-vl-custom-css=".vl-layout {padding:0} .vl-region{padding:0} .vl-region:first-child{padding:0} :host #vl-side-sheet {padding:0} :host {--vl-side-sheet-width: 600px;}"
+            >
+                <h4 is="vl-h4" data-vl-has-border={true}>
+                    Kies uit kantoren
+                </h4>
+                <VlCascader items={nodeData} itemListFn={getItemList} templates={cascaderItemTemplates}></VlCascader>
             </vl-side-sheet>
         </main>
     );
