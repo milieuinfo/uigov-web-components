5d00f7c5f4005d6f7897600f1f964bdace206988
Kristof Spaas <kristof.spaas@vlaanderen.be>
2023-11-17 16:33:11 +0100

    feat: UIG-2750 - Form CSP validatie
    Playgrounds CSP compliant en e2e testen
    Build uitgebreid

---
+++ apps/playground-react-e2e/cypress.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+    fileServerFolder: '.',
+    fixturesFolder: './src/fixtures',
+    modifyObstructiveCode: false,
+    video: true,
+    videosFolder: '../../dist/cypress/apps/playground-react-e2e/videos',
+    screenshotsFolder: '../../dist/cypress/apps/playground-react-e2e/screenshots',
+    chromeWebSecurity: false,
+    reporter: '../../node_modules/cypress-multi-reporters',
+    reporterOptions: {
+        configFile: 'reporter-config.json',
+    },
+    retries: 3,
+    e2e: {
+        specPattern: './src/e2e/**/*.cy.{js,jsx,ts,tsx}',
+        supportFile: './src/support/e2e.ts',
+    },
+    experimentalCspAllowList: ['default-src'],
+});
--- resources/ci/scripts/e2e-tests-storybook.sh
+++ resources/ci/scripts/e2e-tests-storybook.sh
@@ -5,7 +5,19 @@ set -e
 
 echo 'RUNNING SCRIPT: e2e-tests-storybook.sh'
 cd uigov-web-components
+
 echo "npm install - no 'ci' to avoid the clean"
 npm install --save-exact
+
 echo "run the e2e tests"
 npm run storybook:ci-test
+
+echo "cd dist/cypress/apps/storybook-e2e/mochawesome-reports"
+cd dist/cypress/apps/storybook-e2e/mochawesome-reports
+echo "ls -la"
+ls -la
+cd ../../../../..
+echo "mochawesome report Storybook"
+mkdir dist/cypress/apps/storybook-e2e/mochawesome
+npx mochawesome-merge dist/cypress/apps/storybook-e2e/mochawesome-reports/*.json > dist/cypress/apps/storybook-e2e/mochawesome/output.json
+npx marge dist/cypress/apps/storybook-e2e/mochawesome/output.json --reportDir dist/cypress/apps/storybook-e2e/mochawesome --inline --reportFilename index.html --reportPageTitle "Storybook e2e tests" --reportTitle "UIGOV Webcomponents - Storybook e2e tests"
