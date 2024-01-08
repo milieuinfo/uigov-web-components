401f44015282507cfdb8e419eed55d1d5bf56dfe
Dante Solis <dantevs@icloud.com>
2024-01-08 17:20:07 +0100

    feat: UIG-2757 - debounce voorbeeld / herstructurering van de voorbeelden

--- apps/storybook-e2e/src/e2e/elements/input-field/vl-input-field.stories.cy.ts
+++ apps/storybook-e2e/src/e2e/elements/input-field/vl-input-field.stories.cy.ts
@@ -2,50 +2,7 @@ const inputFieldUrl = 'http://localhost:8080/iframe.html?id=elements-input-field
 
 describe('story vl-input-field - default', () => {
     it('should be accessible', () => {
-        cy.visitWithA11y(`${inputFieldUrl}`);
-        cy.get('[is="vl-input-field"]');
-        cy.checkA11y('[is="vl-input-field"]');
-        cy.get('[is="vl-input-field"]').should('have.class', 'vl-input-field');
-    });
-
-    it('should contain an input field', () => {
-        cy.visit(`${inputFieldUrl}`);
-        cy.get('[is="vl-input-field"]').should('have.class', 'vl-input-field');
-    });
-
-    it('should contain an input field with a block state', () => {
-        cy.visit(`${inputFieldUrl}&args=block:true`);
-        cy.get('[is="vl-input-field"]')
-            .should('have.class', 'vl-input-field')
-            .should('have.class', 'vl-input-field--block');
-    });
-
-    it('should contain an input field with an error state ', () => {
-        cy.visit(`${inputFieldUrl}&args=error:true`);
-        cy.get('[is="vl-input-field"]')
-            .should('have.class', 'vl-input-field')
-            .should('have.class', 'vl-input-field--error');
-    });
-
-    it('should contain an input field with an success state ', () => {
-        cy.visit(`${inputFieldUrl}&args=success:true`);
-        cy.get('[is="vl-input-field"]')
-            .should('have.class', 'vl-input-field')
-            .should('have.class', 'vl-input-field--success');
-    });
-
-    it('should contain an input field with an disabled state ', () => {
-        cy.visitWithA11y(`${inputFieldUrl}&args=small:true;disabled:true`);
-        cy.get('[is="vl-input-field"]')
-            .should('have.class', 'vl-input-field')
-            .should('have.class', 'vl-input-field--disabled')
-            .should('have.attr', 'disabled');
-    });
-
-    it('should contain an input field with a small state', () => {
-        cy.visit(`${inputFieldUrl}&args=small:true`);
-        cy.get('[is="vl-input-field"]')
-            .should('have.class', 'vl-input-field')
-            .should('have.class', 'vl-input-field--small');
+        cy.visit(inputFieldUrl);
+        cy.get('input[is="vl-input-field"]').should('have.class', 'vl-input-field');
     });
 });
--- libs/common/utilities/src/index.ts
+++ libs/common/utilities/src/index.ts
@@ -17,7 +17,6 @@ export {
     sleep,
     awaitUntil,
     unwrap,
-    deferred,
     debounce,
     returnNotEmptyString,
     returnNumber,
