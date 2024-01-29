6e9d00cbdd4180dc727443ff0fbd810bb53abd0d
Kris Speltincx <kris.speltincx@vlaanderen.be>
2024-01-29 15:18:54 +0100

    feat: build verbeteringen

--- resources/ci/scripts/build-apps-and-libs-docker.sh
+++ resources/ci/scripts/build-apps-and-libs-docker.sh
@@ -7,4 +7,4 @@ echo 'RUNNING SCRIPT: build-apps-and-libs-docker.sh'
 export BUILD_SCRIPT=uigov-web-components/resources/ci/scripts/build-apps-and-libs.sh
 cd resources/ci
 echo 'docker-compose run'
-docker-compose run
+docker-compose run build
--- resources/ci/scripts/checkout-install-and-cache-docker.sh
+++ resources/ci/scripts/checkout-install-and-cache-docker.sh
@@ -6,5 +6,5 @@ set -e
 echo 'RUNNING SCRIPT: checkout-install-and-cache-docker.sh'
 export BUILD_SCRIPT=uigov-web-components/resources/ci/scripts/checkout-install-and-cache.sh
 cd resources/ci
-echo 'docker-compose up -V'
-docker-compose up -V --abort-on-container-exit --exit-code-from build
+echo 'docker-compose run'
+docker-compose run build
