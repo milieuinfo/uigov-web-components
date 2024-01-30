c48302ee5e83ecefbc604f2ad5b0560f1286aae3
Kris Speltincx <kris.speltincx@vlaanderen.be>
2024-01-30 10:50:37 +0100

    feat: build verbeteringen

--- resources/ci/scripts/build-apps-and-libs.sh
+++ resources/ci/scripts/build-apps-and-libs.sh
@@ -7,7 +7,7 @@ echo 'RUNNING SCRIPT: build-apps-and-libs.sh'
 cd uigov-web-components
 
 echo "npm install - no 'ci' to avoid the clean"
-npm install --save-exact  &> /dev/null
+npm install --save-exact &> /dev/null
 if [ $? -eq 0 ]
   then
     echo "npm install - success"
@@ -17,16 +17,64 @@ fi
 
 echo 'BUILDING'
 echo "build storybook"
-npx nx build-storybook storybook
+npx nx build-storybook storybook &> /dev/null
+if [ $? -eq 0 ]
+  then
+    echo "build storybook - success"
+  else
+    echo "build storybook - error" >&2
+fi
+
 echo "build playground-legacy"
-npx nx build playground-legacy
+npx nx build playground-legacy &> /dev/null
+if [ $? -eq 0 ]
+  then
+    echo "build playground-legacy - success"
+  else
+    echo "build playground-legacy - error" >&2
+fi
+
 echo "build playground-lit"
-npx nx build playground-lit
+npx nx build playground-lit &> /dev/null
+if [ $? -eq 0 ]
+  then
+    echo "build playground-lit - success"
+  else
+    echo "build playground-lit - error" >&2
+fi
+
 echo "build playground-native"
-npx nx build playground-native
+npx nx build playground-native &> /dev/null
+if [ $? -eq 0 ]
+  then
+    echo "build playground-native - success"
+  else
+    echo "build playground-native - error" >&2
+fi
+
 echo "build playground-react"
-npx nx build playground-react
+npx nx build playground-react &> /dev/null
+if [ $? -eq 0 ]
+  then
+    echo "build playground-react - success"
+  else
+    echo "build playground-react - error" >&2
+fi
+
 echo "build libraries"
-npm run build:all
+npm run build:all &> /dev/null
+if [ $? -eq 0 ]
+  then
+    echo "build libraries - success"
+  else
+    echo "build libraries - error" >&2
+fi
+
 echo "build fat-lib"
-npm run build-fat-lib
+npm run build-fat-lib &> /dev/null
+if [ $? -eq 0 ]
+  then
+    echo "build fat-lib - success"
+  else
+    echo "build fat-lib - error" >&2
+fi
