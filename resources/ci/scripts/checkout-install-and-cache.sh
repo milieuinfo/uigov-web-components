bd33dbcdba7f7fe427cb4436fc8522351e7ee1ec
Kris Speltincx <kris.speltincx@vlaanderen.be>
2024-01-30 15:19:33 +0100

    feat: build verbeteringen

--- resources/ci/scripts/checkout-install-and-cache.sh
+++ resources/ci/scripts/checkout-install-and-cache.sh
@@ -10,14 +10,10 @@ echo "npm ci"
 npm ci --maxsockets 5 &> output-buffer.txt
 if [ $? -eq 0 ]
   then
-    echo "npm ci - success"
-    echo "npm ci - success - begin of output" >&2
-    sync
-    cat output-buffer.txt
-    echo "npm ci - success - end of output" >&2
+#    echo "npm ci - success"
+    echo "npm ci - success - output of buffer" >&2
+    cat output-buffer.txt >&2
   else
-    echo "npm ci - error - begin of output" >&2
-    sync
-    cat output-buffer.txt
-    echo "npm ci - error - end of output" >&2
+    echo "npm ci - error - output of buffer" >&2
+    cat output-buffer.txt >&2
 fi
