bfcab3257957d2fda28c02bec5582898f3ac3268
Kris Speltincx <kris.speltincx@vlaanderen.be>
2024-01-30 14:42:43 +0100

    feat: build verbeteringen

--- resources/ci/scripts/checkout-install-and-cache.sh
+++ resources/ci/scripts/checkout-install-and-cache.sh
@@ -7,10 +7,15 @@ echo 'RUNNING SCRIPT: checkout-install-and-cache.sh'
 cd uigov-web-components
 
 echo "npm ci"
-npm ci --maxsockets 5 &> /dev/null
+npm ci --maxsockets 5 &> output-buffer.txt
 if [ $? -eq 0 ]
   then
     echo "npm ci - success"
+    echo "npm ci - success - begin of output" >&2
+    cat output-buffer.txt
+    echo "npm ci - success - end of output" >&2
   else
-    echo "npm ci - error" >&2
+    echo "npm ci - error - begin of output" >&2
+    cat output-buffer.txt
+    echo "npm ci - error - end of output" >&2
 fi
