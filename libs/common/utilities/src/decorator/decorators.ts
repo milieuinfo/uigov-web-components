0fc2e9426399d044bc9aea571acb979dd5a1ead0
Kris Speltincx <kris.speltincx@vlaanderen.be>
2024-02-05 14:43:52 +0100

    feat: UIG-2773 - Lit property decorators
    - package.json ge-update naar de nieuwste versies
       -> qlik en stardust upgrade - afgestemt met Sander B.
       -> storybook-addon-mock mag niet ge-update worden
    - vl-step en vl-steps: properties omgezet naar @property
    - components/next/cascader en components/next/steps bij in fat-js
    - CustomElementClass verwijderd om de wct testen te doen slagen
    - override van map in VlSpitAction geeft fout

--- libs/common/utilities/src/decorator/decorators.ts
+++ libs/common/utilities/src/decorator/decorators.ts
@@ -1,34 +1,78 @@
+import { BaseElementOfType } from '../base/base.element';
 import { UigConfig } from '../config/uig-config';
 import { defineWebComponent } from '../util/utils';
 
+// types en opzet overgenomen van Lit
+//  -> https://github.com/lit/lit/blob/main/packages/reactive-element/src/decorators/custom-element.ts
+
+type Constructor<T> = {
+    new (...args: any[]): T;
+};
+
+export type CustomElementDecorator = {
+    // legacy
+    (cls: any): void;
+    // standard
+    (target: any, context: ClassDecoratorContext<Constructor<HTMLElement>>): void;
+};
+
 export const webComponent =
-    (tagName: string, options?: ElementDefinitionOptions) =>
-    // eslint-disable-next-line @typescript-eslint/ban-types
-    (constructor: Function): any => {
-        defineWebComponent(constructor, tagName, options);
+    (tagName: string, options?: ElementDefinitionOptions): CustomElementDecorator =>
+    (classOrTarget: any, context?: ClassDecoratorContext<Constructor<HTMLElement>>) => {
+        if (context !== undefined) {
+            context.addInitializer(() => {
+                defineWebComponent(classOrTarget as CustomElementConstructor, tagName, options);
+            });
+        } else {
+            defineWebComponent(classOrTarget as CustomElementConstructor, tagName, options);
+        }
     };
 
 // variant waaraan een custom registratie methode wordt meegegeven
 export const webComponentCustom =
-    (customRegistration: () => any) =>
-    // eslint-disable-next-line @typescript-eslint/ban-types
-    (constructor: Function): any => {
-        customRegistration();
+    (customRegistration: () => any): CustomElementDecorator =>
+    (classOrTarget: any, context?: ClassDecoratorContext<Constructor<HTMLElement>>) => {
+        if (context !== undefined) {
+            context.addInitializer(() => {
+                customRegistration();
+            });
+        } else {
+            customRegistration();
+        }
     };
 
 // TODO: deze Promised variant zou niet nodig moeten zijn
 //  -> misschien is het ook niet meer nodig omdat kind-web-componenten nu ook @webComponent
 //     gebruiken en als ze dus geïmporteerd worden al geregistreerd worden: te testen / verifiëren
 export const webComponentPromised =
-    <T>(promises: Iterable<T | PromiseLike<T>>, tagName: string, options?: ElementDefinitionOptions) =>
-    // eslint-disable-next-line @typescript-eslint/ban-types
-    (constructor: Function): any => {
-        if (customElements.get(tagName)) {
-            if (UigConfig.getPreferences().logWebComponentRegistration) {
-                console.debug(`${tagName} werd reeds geregistreerd`);
-            }
+    <T>(
+        promises: Iterable<T | PromiseLike<T>>,
+        tagName: string,
+        options?: ElementDefinitionOptions
+    ): CustomElementDecorator =>
+    (classOrTarget: any, context?: ClassDecoratorContext<Constructor<HTMLElement>>) => {
+        if (context !== undefined) {
+            context.addInitializer(() => {
+                if (customElements.get(tagName)) {
+                    if (UigConfig.getPreferences().logWebComponentRegistration) {
+                        console.debug(`${tagName} werd reeds geregistreerd`);
+                    }
+                } else {
+                    Promise.all(promises).then(() =>
+                        defineWebComponent(classOrTarget as CustomElementConstructor, tagName, options)
+                    );
+                }
+            });
         } else {
-            Promise.all(promises).then(() => defineWebComponent(constructor, tagName, options));
+            if (customElements.get(tagName)) {
+                if (UigConfig.getPreferences().logWebComponentRegistration) {
+                    console.debug(`${tagName} werd reeds geregistreerd`);
+                }
+            } else {
+                Promise.all(promises).then(() =>
+                    defineWebComponent(classOrTarget as CustomElementConstructor, tagName, options)
+                );
+            }
         }
     };
 
@@ -36,14 +80,29 @@ export const webComponentPromised =
 //  -> misschien is het ook niet meer nodig omdat kind-web-componenten nu ook @webComponent
 //     gebruiken en als ze dus geïmporteerd worden al geregistreerd worden: te testen / verifiëren
 export const webComponentConditional =
-    (defined: string, tagName: string, options?: ElementDefinitionOptions) =>
-    // eslint-disable-next-line @typescript-eslint/ban-types
-    (constructor: Function): any => {
-        if (customElements.get(tagName)) {
-            if (UigConfig.getPreferences().logWebComponentRegistration) {
-                console.debug(`${tagName} werd reeds geregistreerd`);
-            }
+    (defined: string, tagName: string, options?: ElementDefinitionOptions): CustomElementDecorator =>
+    (classOrTarget: any, context?: ClassDecoratorContext<Constructor<HTMLElement>>) => {
+        if (context !== undefined) {
+            context.addInitializer(() => {
+                if (customElements.get(tagName)) {
+                    if (UigConfig.getPreferences().logWebComponentRegistration) {
+                        console.debug(`${tagName} werd reeds geregistreerd`);
+                    }
+                } else {
+                    window.customElements
+                        .whenDefined(defined)
+                        .then(() => defineWebComponent(classOrTarget as CustomElementConstructor, tagName, options));
+                }
+            });
         } else {
-            window.customElements.whenDefined(defined).then(() => defineWebComponent(constructor, tagName, options));
+            if (customElements.get(tagName)) {
+                if (UigConfig.getPreferences().logWebComponentRegistration) {
+                    console.debug(`${tagName} werd reeds geregistreerd`);
+                }
+            } else {
+                window.customElements
+                    .whenDefined(defined)
+                    .then(() => defineWebComponent(classOrTarget as CustomElementConstructor, tagName, options));
+            }
         }
     };
--- libs/components/src/next/steps/vl-step.component.ts
+++ libs/components/src/next/steps/vl-step.component.ts
@@ -2,8 +2,8 @@ import { BaseLitElement, registerWebComponents, VL } from '@domg-wc/common-utili
 import { vlElementsStyle } from '@domg-wc/elements';
 import { resetStyle } from '@domg/govflanders-style/common';
 import { stepsStyle } from '@domg/govflanders-style/component';
-import { CSSResult, html, PropertyDeclarations, TemplateResult } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { CSSResult, html, TemplateResult } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
 import { classMap } from 'lit/directives/class-map.js';
 import { VlAccordionComponent } from '../../accordion/vl-accordion.component';
 import stepUigStyle from './vl-step.uig-css';
@@ -13,11 +13,15 @@ declare const vl: VL;
 @customElement('vl-step-next')
 export class VlStepComponent extends BaseLitElement {
     // Attributen
-    private type: string | null = null;
-    private toggleable = false;
+    @property({ type: String, attribute: 'data-vl-type', reflect: true })
+    accessor type: string | null = null;
+    @property({ type: Boolean, attribute: 'data-vl-toggleable', reflect: true })
+    accessor toggleable = false;
 
     // Private properties
-    private isTitleAnnotationSlotAssigned = true;
+    @property({ attribute: false })
+    accessor isTitleAnnotationSlotAssigned = true;
+
     private customCSSStyleSheet = new CSSStyleSheet();
 
     static {
@@ -26,14 +30,6 @@ export class VlStepComponent extends BaseLitElement {
 
     static get styles(): (CSSResult | CSSResult[])[] {
         return [resetStyle, vlElementsStyle, stepsStyle, stepUigStyle];
-    }
-
-    static get properties(): PropertyDeclarations {
-        return {
-            type: { type: String, attribute: 'data-vl-type', reflect: true },
-            toggleable: { type: Boolean, attribute: 'data-vl-toggleable', reflect: true },
-            isTitleAnnotationSlotAssigned: { attribute: false },
-        };
     }
 
     connectedCallback(): void {
