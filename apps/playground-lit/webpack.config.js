4332284a2430f4935e93102d9f6ca22b489390ac
Kristof Spaas <kristof.spaas@vlaanderen.be>
2024-03-22 11:10:34 +0100

    feat: UIG-2941 - integrator - csp test form-demo
    CSP test van form-demo toegevoegd in integrator app
    playground-lit-e2e app verwijderd
    webpack configs playgrounds aangepast

--- apps/playground-lit/webpack.config.js
+++ apps/playground-lit/webpack.config.js
@@ -4,12 +4,5 @@ const { composePlugins, withNx, withWeb } = require('@nx/webpack');
 module.exports = composePlugins(withNx(), withWeb(), (config) => {
     // Update the webpack config as needed here.
     // e.g. `config.plugins.push(new MyPlugin())`
-    config.devServer = {
-        ...config.devServer,
-        headers: {
-            'Content-Security-Policy-Report-Only': `default-src 'self' cdn.omgeving.vlaanderen.be; report-uri /csp-report;`,
-        },
-    };
-
     return config;
 });
--- apps/playground-react/webpack.config.js
+++ apps/playground-react/webpack.config.js
@@ -5,12 +5,5 @@ const { withReact } = require('@nx/react');
 module.exports = composePlugins(withNx(), withReact(), (config) => {
     // Update the webpack config as needed here.
     // e.g. `config.plugins.push(new MyPlugin())`
-    config.devServer = {
-        ...config.devServer,
-        headers: {
-            'Content-Security-Policy-Report-Only': `default-src 'self' cdn.omgeving.vlaanderen.be; report-uri /csp-report;`,
-        },
-    };
-
     return config;
 });
