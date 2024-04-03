c5dbe9db47ad07c3df8e69ca53f2be3447ab45b3
Karim Dehbi <karim.dehbi@gmail.com>
2024-04-03 18:35:39 +0200

    fix: UIG-2982 - vl-data-table - responsive layout regressie gestroomlijnd
    Vroeger gebruikte DV o.a. relative positioning om responsiveness in de data-table te bekomen en aan onze kant patchen wij CSS om dynamische functionaliteit te kunnen toevoegen. Na de upgrade naar recentste DV styling wordt er nu gebruikt gemaakt van flexbox. Nu nemen we die over en is het ook niet langer nodig om de CSS te patchen aan onze kant.

--- libs/elements/src/data-table/vl-data-table.uig-css.ts
+++ libs/elements/src/data-table/vl-data-table.uig-css.ts
@@ -21,38 +21,5 @@ const styles: CSSResult = css`
         background: #cbd2d9;
         color: var(--vl-theme-fg-color-70);
     }
-
-    tr[data-details-id] {
-        display: table-row;
-    }
-
-    @media screen and (max-width: 500px) {
-        .vl-data-table--collapsed-m td,
-        .vl-data-table--collapsed-s td,
-        .vl-data-table--collapsed-xs td {
-            padding-left: 1rem;
-            padding-top: 3.6rem;
-        }
-        .vl-data-table--collapsed-m td::before,
-        .vl-data-table--collapsed-s td::before,
-        .vl-data-table--collapsed-xs td::before {
-            width: 90%;
-            padding-right: 0;
-        }
-
-        .vl-data-table--collapsed-xs tr[data-details-id] {
-            display: block;
-        }
-    }
-    @media screen and (max-width: 767px) {
-        .vl-data-table--collapsed-s tr[data-details-id] {
-            display: block;
-        }
-    }
-    @media screen and (max-width: 1023px) {
-        .vl-data-table--collapsed-m tr[data-details-id] {
-            display: block;
-        }
-    }
 `;
 export default styles;
