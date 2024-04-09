a6d36fe4692e033c0d8710dead9277afbed12215
Karim Dehbi <karim.dehbi@gmail.com>
2024-04-09 10:15:53 +0200

    fix: UIG-2991 - vl-rich-data - falsy checks verbeterd
    Falsy checks in de rich-data component verbeterd.
    Storybook test gemigreerd naar component test.

--- apps/storybook-e2e/src/e2e/components/rich-data/vl-rich-data.stories.cy.ts
+++ apps/storybook-e2e/src/e2e/components/rich-data/vl-rich-data.stories.cy.ts
@@ -1,22 +1,10 @@
 const richDataUrl = 'http://localhost:8080/iframe.html?args=&id=components-rich-data--rich-data-pager&viewMode=story';
 
-describe('story vl-rich-data', () => {
-    it('as a user, I can see the rich-data pager', () => {
+describe('story - vl-rich-data', () => {
+    it('should render', () => {
         cy.visit(`${richDataUrl}`);
-        cy.get('vl-pager')
-            .shadow()
-            .find('li[id=bounds]')
-            .children('strong')
-            .then((child) => {
-                expect(child[0]).to.contain('1-5');
-            });
-        cy.get('vl-pager').shadow().find('li[data-vl-pager-page=2]').click();
-        cy.get('vl-pager')
-            .shadow()
-            .find('li[id=bounds]')
-            .children('strong')
-            .then((child) => {
-                expect(child[0]).to.contain('6-10');
-            });
+
+        cy.get('vl-rich-data').shadow().find('div[is="vl-grid"]');
+        cy.get('vl-pager').shadow().find('li[id=bounds]');
     });
 });
