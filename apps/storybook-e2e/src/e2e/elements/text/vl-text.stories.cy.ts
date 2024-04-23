08bb90c3eb8432da5765a1266c42ef7ada7e5bea
Karim Dehbi <karim.dehbi@gmail.com>
2024-04-23 16:48:48 +0200

    feat: UIG-3002 - vl-text - kleur attributen toegevoegd
    Storybook & cypress testen verbeterd.

--- apps/storybook-e2e/src/e2e/elements/text/vl-text.stories.cy.ts
+++ apps/storybook-e2e/src/e2e/elements/text/vl-text.stories.cy.ts
@@ -1,13 +1,6 @@
-describe('story vl-text', () => {
-    it('should contain a text', () => {
+describe('story - vl-text', () => {
+    it('should render', () => {
         cy.visit('http://localhost:8080/iframe.html?id=elements-text--text-default&viewMode=story');
-        cy.getDataCy('text-default').contains('Text');
-    });
-
-    it('should contain a visually hidden text', () => {
-        cy.visit(
-            'http://localhost:8080/iframe.html?id=elements-text--text-default&viewMode=story&args=hidden:true;content:Text%20is%20visually%20hidden'
-        );
-        cy.getDataCy('text-default').should('have.class', 'vl-u-visually-hidden').contains('Text is visually hidden');
+        cy.get('span[is="vl-text"]').contains('Tekst');
     });
 });
