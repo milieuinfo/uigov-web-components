85ef27e2c9f2350b8e9e167086f0d68a3bf1a1db
Karim Dehbi <karim.dehbi@gmail.com>
2024-05-27 13:27:13 +0200

    fix: UIG-3014 - vl-modal - allow-overflow attribute werking rechtgezet

--- libs/components/src/modal/vl-modal.uig-css.ts
+++ libs/components/src/modal/vl-modal.uig-css.ts
@@ -1,7 +1,8 @@
 import { css, CSSResult } from 'lit';
 
 const styles: CSSResult = css`
-    :host([data-vl-allow-overflow]) dialog {
+    :host([data-vl-allow-overflow]) dialog,
+    :host([data-vl-allow-overflow]) dialog .vl-modal-dialog__wrapper {
         overflow: visible;
     }
 
