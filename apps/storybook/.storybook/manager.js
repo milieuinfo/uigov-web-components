7d43a7d05a40e8622c39bd53bce4396a0f6c48e6
Karim Dehbi <karim.dehbi@gmail.com>
2024-06-05 13:18:39 +0200

    feat: UIG-3022 - storybook - deprecated badge renderen wanneer tag aanwezig is
    TODO:
    
    - breedte sidebar bijwerken
    - deprecated stories zonder .mdx kunnen potentiëel probleem geven - te bekijken
    - om `deprecated` te detecteren dienen we die toe te voegen in de naam van het story component; dit heeft als gevolg dat alle bijhorende storybook testen ook aangepast moeten worden

--- apps/storybook/.storybook/manager.js
+++ apps/storybook/.storybook/manager.js
@@ -1,7 +1,43 @@
 import { addons } from '@storybook/addons';
 import theme from './voTheme';
+import React from 'react';
 
 addons.setConfig({
     theme,
     enableShortcuts: false,
+    sidebar: {
+        renderLabel: (storyData) => {
+            const { name, id, type, parameters = {} } = storyData;
+            const statusRegex = /\[([^)]+)]/gi;
+            const [statusMatch, statusType] = statusRegex.exec(name) || [];
+            if (statusMatch && statusType === 'deprecated') {
+                return (
+                    <div
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                            width: '100%',
+                            height: '16px',
+                        }}
+                    >
+                        <div>{name.replace(statusMatch, '').trim()}</div>
+                        <div
+                            style={{
+                                color: '#d83a52',
+                                border: '1px solid #d83a52',
+                                borderRadius: '0.3rem',
+                                padding: '0.2rem 0.4rem',
+                                marginRight: '1.6rem',
+                            }}
+                        >
+                            Deprecated
+                        </div>
+                    </div>
+                );
+            } else {
+                return null;
+            }
+        },
+    },
 });
