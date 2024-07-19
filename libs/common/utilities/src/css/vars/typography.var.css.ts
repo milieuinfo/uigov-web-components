6bfb9c184b2cfce515ac8dfc378073f2cda7b493
Karim Dehbi <karim.dehbi@gmail.com>
2024-07-19 11:50:36 +0200

    feat: UIG-2914 - storybook - migratiegids aangemaakt
    typography.var.scss aangepast met font-size mappings & DV referenties

--- libs/common/utilities/src/css/vars/typography.var.css.ts
+++ libs/common/utilities/src/css/vars/typography.var.css.ts
@@ -8,13 +8,17 @@ const styles: CSSResult = css`
         --vl-font: ${unsafeCSS(fontFamily)}, sans-serif;
         --vl-accent-font: ${unsafeCSS(serifFontFamily)}, serif;
 
-        --vl-font-size: 1.8rem;
-        --vl-font-size--mobile: 1.6rem;
-        --vl-font-size--small: 1.6rem;
-        --vl-font-size--large: 2rem;
+        --vl-font-size--xxlarge: 2.8rem; // DV: $vl-base-font-size--xxlarge
+        --vl-font-size--xlarge: 2.2rem; // DV: $vl-base-font-size--xlarge
+        --vl-font-size--large: 2rem; // DV: $vl-base-font-size--large
+        --vl-font-size: 1.8rem; // DV: $vl-base-font-size
+        --vl-font-size--mobile: 1.6rem; // Dv: $vl-base-mobile-font-size
+        --vl-font-size--small: 1.6rem; // DV: $vl-base-font-size--small
+        --vl-font-size--xsmall: 1.4rem; // DV: $vl-base-font-size--xsmall
+        --vl-font-size--xxsmall: 1.2rem; // DV: $vl-base-font-size--xxsmall
 
         --vl-line-height: 1.5; // DV: $vl-base-line-height
-        --vl-line-height--mobile: 1.33;
+        --vl-line-height--mobile: 1.33; // DV: $vl-base-mobile-line-height
 
         /* Icon font */
         --vl-icon-font: ${unsafeCSS(iconFontFamily)};
