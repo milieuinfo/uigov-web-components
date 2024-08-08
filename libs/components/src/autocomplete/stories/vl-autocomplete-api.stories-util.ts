21ffe8484a7852d6cd453480e4a7ee67a7ffcda2
Karim Dehbi <karim.dehbi@gmail.com>
2024-08-08 10:12:01 +0200

    fix: UIG-3063 - vl-autocomplete - API-story bijgewerkt met huidige url

--- libs/components/src/autocomplete/stories/vl-autocomplete-api.stories-util.ts
+++ libs/components/src/autocomplete/stories/vl-autocomplete-api.stories-util.ts
@@ -1,6 +1,6 @@
 export async function fetchDataFromApiCall(autocomplete: any, searchTerm: any) {
     const result = await fetch(
-        `https://loc.geopunt.be/geolocation/suggestion?q=${searchTerm}&c=${autocomplete.maxSuggestions}`
+        `https://geo.api.vlaanderen.be/geolocation/suggestion?q=${searchTerm}&c=${autocomplete.maxSuggestions}`
     );
     const responseBody = await result.json();
     autocomplete.matches = responseBody.SuggestionResult.map((obj: string) => ({
