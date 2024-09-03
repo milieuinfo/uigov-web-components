533a18cb084e31b5f2eb1b1ede649fec9db96748
Karim Dehbi <karim.dehbi@gmail.com>
2024-09-03 14:39:53 +0200

    aanbevelingen toevoegen ivm validatie informatie / hints + duidelijker default foutboodschappen meegeven

--- libs/form/src/next/upload/vl-upload.defaults.ts
+++ libs/form/src/next/upload/vl-upload.defaults.ts
@@ -6,9 +6,10 @@ export const uploadDefaults = {
     acceptedFiles: '' as string,
     autoProcess: false as boolean,
     disallowDuplicates: false as boolean,
-    errorMessageAcceptedFiles: 'Dit bestandstype is niet toegestaan.' as string,
-    errorMessageFilesize: 'Het bestand mag maximaal {{maxFilesize}} MB zijn.' as string,
-    errorMessageMaxFiles: 'Je kan maximaal {{maxFiles}} bestand(en) uploaden.' as string,
+    errorMessageAcceptedFiles: 'Dit bestand is niet toegevoegd. Dit bestandstype is niet toegestaan.' as string,
+    errorMessageFilesize: 'Dit bestand is niet toegevoegd. Het bestand mag maximaal {{maxFilesize}} MB zijn.' as string,
+    errorMessageMaxFiles:
+        'Dit bestand is niet toegevoegd. Je kan maximaal {{maxFiles}} bestand(en) uploaden.' as string,
     maxFiles: 1 as number,
     maxSize: 2 as number,
     subTitle: 'Sleep de bijlage naar hier om toe te voegen' as string,
