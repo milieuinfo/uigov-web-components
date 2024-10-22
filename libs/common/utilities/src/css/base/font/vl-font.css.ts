cb8e2456dfee93b3035d46c90e8d67364cac9efb
Kris Speltincx <kris.speltincx@vlaanderen.be>
2024-10-22 12:46:01 +0200

    feat: UIG-2907 - generate-icon-files omgezet naar ts

--- libs/common/utilities/src/css/base/font/vl-font.css.ts
+++ libs/common/utilities/src/css/base/font/vl-font.css.ts
@@ -1,15 +1,10 @@
 import fontsRawCss from '!!raw-loader!./vl-font.raw.css';
 import { css, CSSResult, unsafeCSS } from 'lit';
 
-// Flanders fonts
-const fontFamily = 'vl-flanders-art-sans';
+const sansFontFamily = 'vl-flanders-art-sans';
 const serifFontFamily = 'vl-flanders-art-serif';
 const iconFontFamily = 'vl-flanders-icon-classic';
-
-// font locations
-const rootFontLocation = 'https://cdn.omgeving.vlaanderen.be/domg/govflanders-font/22.0.2';
-const flandersFontLocation = `${rootFontLocation}/flanders`;
-const iconFontLocation = `${rootFontLocation}/iconfont`;
+const fontLocation = 'https://cdn.omgeving.vlaanderen.be/domg/govflanders-font/22.0.2';
 
 const fontFace = (
     fontFamily: string,
@@ -29,20 +24,20 @@ const fontFace = (
 export const fontStyles: CSSResult = css`
     ${unsafeCSS(fontsRawCss)}
 
-    ${fontFace(fontFamily, `${flandersFontLocation}/sans/FlandersArtSans-Light`, 300, 'normal')}
-    ${fontFace(fontFamily, `${flandersFontLocation}/sans/FlandersArtSans-Regular`, 400, 'normal')}
-    ${fontFace(fontFamily, `${flandersFontLocation}/sans/FlandersArtSans-Medium`, 500, 'normal')}
-    ${fontFace(fontFamily, `${flandersFontLocation}/sans/FlandersArtSans-Bold`, 700, 'normal')}
+    ${fontFace(sansFontFamily, `${fontLocation}/flanders/sans/FlandersArtSans-Light`, 300, 'normal')}
+    ${fontFace(sansFontFamily, `${fontLocation}/flanders/sans/FlandersArtSans-Regular`, 400, 'normal')}
+    ${fontFace(sansFontFamily, `${fontLocation}/flanders/sans/FlandersArtSans-Medium`, 500, 'normal')}
+    ${fontFace(sansFontFamily, `${fontLocation}/flanders/sans/FlandersArtSans-Bold`, 700, 'normal')}
 
-    ${fontFace(fontFamily, `${flandersFontLocation}/italic/FlandersArtSans-Light`, 300, 'italic')}
-    ${fontFace(fontFamily, `${flandersFontLocation}/italic/FlandersArtSans-Regular`, 400, 'italic')}
-    ${fontFace(fontFamily, `${flandersFontLocation}/italic/FlandersArtSans-Medium`, 500, 'italic')}
-    ${fontFace(fontFamily, `${flandersFontLocation}/italic/FlandersArtSans-Bold`, 700, 'italic')}
+    ${fontFace(sansFontFamily, `${fontLocation}/flanders/italic/FlandersArtSans-Light`, 300, 'italic')}
+    ${fontFace(sansFontFamily, `${fontLocation}/flanders/italic/FlandersArtSans-Regular`, 400, 'italic')}
+    ${fontFace(sansFontFamily, `${fontLocation}/flanders/italic/FlandersArtSans-Medium`, 500, 'italic')}
+    ${fontFace(sansFontFamily, `${fontLocation}/flanders/italic/FlandersArtSans-Bold`, 700, 'italic')}
 
-    ${fontFace(serifFontFamily, `${flandersFontLocation}/serif/FlandersArtSerif-Light`, 300, 'normal')}
-    ${fontFace(serifFontFamily, `${flandersFontLocation}/serif/FlandersArtSerif-Regular`, 400, 'normal')}
-    ${fontFace(serifFontFamily, `${flandersFontLocation}/serif/FlandersArtSerif-Medium`, 500, 'normal')}
-    ${fontFace(serifFontFamily, `${flandersFontLocation}/serif/FlandersArtSerif-Bold`, 700, 'normal')}
+    ${fontFace(serifFontFamily, `${fontLocation}/flanders/serif/FlandersArtSerif-Light`, 300, 'normal')}
+    ${fontFace(serifFontFamily, `${fontLocation}/flanders/serif/FlandersArtSerif-Regular`, 400, 'normal')}
+    ${fontFace(serifFontFamily, `${fontLocation}/flanders/serif/FlandersArtSerif-Medium`, 500, 'normal')}
+    ${fontFace(serifFontFamily, `${fontLocation}/flanders/serif/FlandersArtSerif-Bold`, 700, 'normal')}
 
-    ${fontFace(iconFontFamily, `${iconFontLocation}/vlaanderen-icon-classic`, 'normal', 'normal')}
+    ${fontFace(iconFontFamily, `${fontLocation}/iconfont/vlaanderen-icon-classic`, 'normal', 'normal')}
 `;
